94a1087de46e8caa904e48661fcc0af8e053a6fa
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-01 21:36:05 +0000

    Add reversed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,8 +7,11 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    
-    return str.split('').reverse().join('');
+    let reversed = '';
+    for (let character of str) {
+        reversed = character + reversed;
+    }
+    return reversed;
 }
 
 module.exports = reverse;
