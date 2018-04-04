21386a75a08c62c4b05de2879ca2082d16ac07c2
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-04 23:01:59 +0000

    Add reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,25 +9,34 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-    var num = [];
-    Math.sign(n);
-    if (Math.sign(n) === 1) {
-        n = n.toString().split('');
-        for (var i = 0; i < n.length; i++) {
-            if ( i > 0 ) {
-                num.push(i);
-            }
-        }
-            return parseInt(num.toString().split('').reverse().join(''));
-        } else {
-            n = n.toString().split('');
-            for (var i = 1; i < n.length; i++) {
-            if ( i > 0 ) {
-                num.push(i);
-            }
-        }
-        return "-" + parseInt(num.toString().split('').reverse().join(''));
-    }
+   const reversed = n.toString().split('').reverse().join('');
+   if ( n < 0 ) {
+       return parseInt(reversed) * -1;
+   }
+   return parseInt(reversed);
 }
 
 module.exports = reverseInt;
+
+
+// function reverseInt(n) {
+//     var num = [];
+//     Math.sign(n);
+//     if (Math.sign(n) === 1) {
+//         n = n.toString().split('');
+//         for (var i = 0; i < n.length; i++) {
+//             if ( i > 0 ) {
+//                 num.push(i);
+//             }
+//         }
+//             return parseInt(num.toString().split('').reverse().join(''));
+//         } else {
+//             n = n.toString().split('');
+//             for (var i = 1; i < n.length; i++) {
+//             if ( i > 0 ) {
+//                 num.push(i);
+//             }
+//         }
+//         return "-" + parseInt(num.toString().split('').reverse().join(''));
+//     }
+// }
