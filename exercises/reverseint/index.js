4c233b1db4115f4a9fb7cba33d0a3f10f150f714
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-04 23:05:25 +0000

    Add reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -10,14 +10,20 @@
 
 function reverseInt(n) {
    const reversed = n.toString().split('').reverse().join('');
-   if ( n < 0 ) {
-       return parseInt(reversed) * -1;
-   }
-   return parseInt(reversed);
+   
+       return parseInt(reversed) * Math.sign(n);
 }
 
 module.exports = reverseInt;
 
+
+// function reverseInt(n) {
+//   const reversed = n.toString().split('').reverse().join('');
+//   if ( n < 0 ) {
+//       return parseInt(reversed) * -1;
+//   }
+//   return parseInt(reversed);
+// }
 
 // function reverseInt(n) {
 //     var num = [];
