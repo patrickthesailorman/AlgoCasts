0deb7e94de61422e5b33b1a89345c127d293ef5a
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-08 22:22:27 +0000

    Add maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,10 +6,24 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-    const chars = {};
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+    
     for (let char of str) {
-        chars[char] = chars[char] + 1
+        if (charMap[char]) {
+            charMap[char] ++;
+        } else {
+            charMap[char] = 1;
+        }
     }
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+   return maxChar;
 }
 
 module.exports = maxChar;
